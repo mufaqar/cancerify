700d3853d2c7a430a6b94b16e66682f530c110a8
anikrauth <anik@getg3ms.com>
2024-07-26 18:41:18 +0600

    fixed

--- components/Home/hero/index.jsx
+++ components/Home/hero/index.jsx
@@ -3,8 +3,10 @@ import SearchForm from "@/components/Home/SearchForm";
 import { useState } from "react";
 
 const index = (props) => {
-  const { mostsearcheds } = props;
+  const { mostsearcheds, hero } = props;
   const [isInputFocused, setIsInputFocused] = useState(false);
+
+  console.log(hero)
   return (
     <section
       className="banner-section-nine bg-theme-color"
@@ -13,9 +15,9 @@ const index = (props) => {
       <div className="auto-container">
         <div className="cotnent-box">
           <div className="title-box">
-            <h3>Find Top Cancer Doctors</h3>
+            <h3>{hero?.heading ? hero?.heading :'Find Top Cancer Doctors'}</h3>
             <div className="text">
-              We've done the research and selected only the world's best oncologists!
+              {hero?.subHeading ? hero?.subHeading : `We've done the research and selected only the world's best oncologists!`}
             </div>
           </div>
 
