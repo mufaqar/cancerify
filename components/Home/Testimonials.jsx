0b2cdfca004c66179d77f2e4ff5e7ad771721cae
cancerify <info@cancerify.com>
2024-10-02 15:31:27 +0600

    added read more in testimonial

--- components/Home/Testimonials.jsx
+++ components/Home/Testimonials.jsx
@@ -1,11 +1,11 @@
-
-
 'use client'
-
+import parseHtml from "@/lib/Parser";
 import Image from "next/image";
 import Slider from "react-slick";
+import {useState} from "react"
 
 const Testimonials = (props) => {
+  const [isExpanded, setIsExpanded] = useState(null);
   const { testimonials } = props;
   const settings = {
     dots: true,
@@ -22,17 +22,36 @@ const Testimonials = (props) => {
       },
     ],
   };
+  const maxLength = 200
 
+  const getShortText = (text, id) => {
+    if(isExpanded === id){
+      return text
+    }else{
+      return text.length > maxLength ? text.slice(0, maxLength) + '...' : text;
+    }
+    
+  };
 
+  console.log(isExpanded)
 
   return (
     <>
-      <Slider {...settings} arrows={false}>
-        {testimonials?.map((item) => (
-          <div className="testimonial-block " key={item.id}>
+      <Slider {...settings} arrows={false} >
+        {testimonials?.map((item, idx) => (
+          <div className="testimonial-block  " key={item.id}>
             <div className="inner-box bg-theme-color reviews_custom">
               <h4 className="title text_white">{item.title}</h4>
-              <div className="text text_white">{item?.content.replace(/(<([^>]+)>)/gi, "")}</div>
+              <div className="text text_white">
+                {getShortText(parseHtml(item?.content.replace(/(<([^>]+)>)/gi, "")), idx)} {''}
+                
+                {
+                  item?.content.replace(/(<([^>]+)>)/gi, "").length > maxLength &&
+                  <span onClick={() => setIsExpanded(isExpanded === idx ? null : idx)} className="">
+                    {isExpanded === idx ? 'Read less' : 'Read more'}
+                  </span>
+                }
+              </div>
               <div className="info-box">
                 {
                   item?.testimonialOptions?.avatar &&
