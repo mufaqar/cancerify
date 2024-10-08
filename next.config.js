fdb6afad95c48afb5fb2550177f01675161479d6
cancerify <info@cancerify.com>
2024-10-09 00:21:42 +0600

    added url image

--- next.config.js
+++ next.config.js
@@ -1,26 +1,30 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'vni.fe7.mytemp.website',
-          },
-          {
-            protocol: 'https',
-            hostname: 'placehold.co',
-          },
-          {
-            protocol: 'https',
-            hostname: 'webdevstudios.com',
-          },
-          {
-            protocol: 'https',
-            hostname: 'secure.gravatar.com',
-          },
-        ],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'vni.fe7.mytemp.website',
       },
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+      },
+      {
+        protocol: 'https',
+        hostname: 'webdevstudios.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'secure.gravatar.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'dash.cancerify.com',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
