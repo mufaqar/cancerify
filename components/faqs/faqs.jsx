146adc9a85278a4c7714bb75e0b4b3c52ddff974
Mufaqar <mufaqar@gmail.com>
2024-10-24 14:39:54 +0500

    faq final

--- components/faqs/faqs.jsx
+++ components/faqs/faqs.jsx
@@ -1,22 +1,45 @@
-import React from 'react'
+"use client";
+
+import React, { useState } from 'react'
 
 const Faqs = () => {
-  return (
-    <section className='faq'>
-        <div className='faq_wrapper'>
-            <h2 className='title'>Frequently Asked Questions</h2>
-            <div className='faqs_list'>
-                {
-                    [1,2,3,4,5].map((item,idx)=>{
-                        <div key={idx}>
-                            {item}
-                        </div>
-                    })
-                }
+    const [open, setOpen] = useState(null);
+    const handleFaq = (idx) => {
+        if (idx === open) {
+            return setOpen(null)
+        }
+        setOpen(idx)
+    }
+    const data = [1, 2, 3, 4, 5]
+    return (
+        <section className='faqs'>
+            <div className='faq_wrapper'>
+                <h2 className='title'>Frequently Asked Questions</h2>
+                <div className='faqs_list'>
+                    {
+                        data.map((item, idx) => {
+                            return (
+                                <div key={idx} className={`faq ${data.length === idx+1 && 'last_faq'}`}>
+                                    <div className={`head`} onClick={() => handleFaq(idx)}>
+                                        <h6>Morem ipsum dolor sit amet, consectetur adipiscing elit. Etiam</h6>
+                                        {
+                                            open === idx ? <button className='menus'><span>_</span></button> :
+                                            <button className='plus'><span>+</span></button>
+                                        }
+                                    </div>
+                                    {
+                                        open === idx && <div className='body'>
+                                            Morem ipsum dolor sit amet, consectetur adipiscing elit. EMorem ipsum dolor sit amet, consectetur adipiscing elit. E
+                                        </div>
+                                    }
+                                </div>
+                            )
+                        })
+                    }
+                </div>
             </div>
-        </div>
-    </section>
-  )
+        </section>
+    )
 }
 
 export default Faqs
